28a3dcbe4f8985661916105a81c7ceb3956b79a9
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-08-25 03:48:39 +1000

    feat: add snippets for `set` builtin

--- src/extension.js
+++ src/extension.js
@@ -3,13 +3,43 @@ const types = require("./types")
 const data = require("./data")
 
 /**
+ * Preprocess snippet
+ * @param {types.SnippetDefinition} snippet A snippet.
+ * 
+ * @returns {Array<types.Snippet>} A result or snippet preprocessing.
+ */
+function preprocessSnippet(snippet) {
+    if (snippet.kind === vscode.CompletionItemKind.Variable)
+        return [
+            types.Snippet.fromRaw({
+                ...snippet,
+                identifier: `set-${snippet.identifier}`,
+                content: `set --export ${snippet.content}`,
+                documentation: {
+                    text: `${snippet.documentation.text} (set via \`set\`)`,
+                    url: snippet.documentation.url
+                }
+            }),
+            types.Snippet.fromRaw(snippet)
+        ]
+
+    return [types.Snippet.fromRaw(snippet)]
+}
+
+/**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
     context.subscriptions.push(
         vscode.languages.registerCompletionItemProvider("fish", {
             provideCompletionItems() {
-                return data.snippets
+                let preprocessedSnippets = []
+                data.snippets
+                    .forEach(snippet => {
+                        preprocessedSnippets = [...preprocessedSnippets, ...preprocessSnippet(snippet)]
+                    })
+
+                return preprocessedSnippets
                     .map(snippet => types.Snippet.fromRaw(snippet).toCompletion())
             }
         })
