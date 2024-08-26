8963b2677a65d9c029e2a5de2d69e8ea550b126e
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-08-27 01:54:30 +1000

    feat(test): enhance Documentation tests
    - use @ts-expect-error
    - add more tests

--- test/Documentation.test.js
+++ test/Documentation.test.js
@@ -7,9 +7,38 @@ const incorrect = Symbol("incorrect value")
 suite("Documentation creation tests", () => {
     test("Fail with incorrect `text` parameter", () => {
         assert.throws(
-            // @ts-ignore
+            // @ts-expect-error
             () => new extension.Documentation(incorrect),
-            "Exception expected for incorrect `text` parameter"
+            "Exception expected for incorrect `text` parameter with incorrect type"
+        )
+
+        assert.throws(
+            // @ts-expect-error
+            () => new extension.Documentation(""),
+            "Exception expected for incorrect `text` parameter with incorrect value"
+        )
+    })
+
+    test("Fail with incorrect `url` parameter", () => {
+        /**
+         * @type {extension.DocumentationDefinition}
+         */
+        const base = {
+            text: "Help.",
+            url: "https://test"
+        }
+
+        assert.throws(
+            () =>
+                // @ts-expect-error
+                new extension.Documentation(base.text, incorrect),
+            "Exception expected for incorrect `url` parameter with incorrect type"
+        )
+
+        assert.throws(
+            () =>
+                new extension.Documentation(base.text, ""),
+            "Exception expected for incorrect `url` parameter with incorrect value"
         )
     })
 
@@ -24,7 +53,8 @@ suite("Documentation creation tests", () => {
 
         assert.deepEqual(
             extension.Documentation.fromRaw(documentationData),
-            new extension.Documentation(documentationData.text, documentationData.url),
+            new extension.Documentation(documentationData.text,
+                documentationData.url),
             messages.fromRaw
         )
     })
@@ -32,7 +62,8 @@ suite("Documentation creation tests", () => {
 
 suite("Documentation conversion tests", () => {
     test("Succeed with correct `toString` result", () => {
-        const documentation = new extension.Documentation("Help.", "https://test")
+        const documentation = new extension.Documentation("Help.",
+            "https://test")
         const footer = "[\`🔗 Read more\`](https://test) | [\`📫 I need help with gum\`](https://github.com/charmbracelet/gum/discussions)"
 
         assert.equal(
