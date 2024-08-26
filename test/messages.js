507315229e8dffdb2d6c822769321c50668b175b
EmilyGraceSeville7cf <EmilyGraceSeville7cfg@gmail.com>
2024-08-27 02:30:41 +1000

    feat(test): standardize error messages

--- test/messages.js
+++ test/messages.js
@@ -1,5 +1,5 @@
-const fromRaw = "Equal contents expected for objects created through constructor and via `fromRaw` method"
-const toString = "Correctly formatted string representation expected for object"
+const fromRaw = "Expected equal contents expected for objects created through constructor and via `fromRaw` method"
+const toString = "Expected correctly formatted string representation expected for object"
 
 module.exports = {
     fromRaw,
